264e711754cf32b5f1429f55c08ea82b93c42369
jgmor13 <jgmor13@gmail.com>
2024-04-04 14:27:29 -0500

    added configs

--- fixVisualization.m
+++ fixVisualization.m
@@ -1,9 +1,9 @@
 % idk why but this seems to fix stuff
 function fixVisualization()
     axis equal; % makes axes proportional
-    xlim([-1.2, 1.2]);
-    ylim([-1.2, 1.2]);
-    zlim([-0.5, 1.8]);
+    xlim([-1.1, .9]);
+    ylim([-1.1, .9]);
+    zlim([-0.5, 1.5]);
     view(3); % force 3D view (fixes "stretching" issues?)
     camlight; % make shaded
     %set(findall(gca, 'Type', 'patch'), 'FaceAlpha', 0.5); % make transparent
--- presentation_animations.m
+++ presentation_animations.m
@@ -7,10 +7,12 @@ config = thetaToConfiguration([0, 0.5, 1, -1.5, 0, 1, 0], setup.Robot);
 % config_a = thetaToConfiguration([0, 0.5, 1, -1.5, 0, 1, 0], setup.Robot);
 % config_b = thetaToConfiguration([0.3, 0.7, 1, -1.2, 0, 0.8, 0], setup.Robot);
 
+test_config1 = thetaToConfiguration([1, 1, 1, 1, 1, pi()/2, pi()/2], setup.Robot);
+test_config2 = thetaToConfiguration([0, 0, 0, 0, 0, 0, .5], setup.Robot);
 config_a = randomConfiguration(setup.Robot);
 config_b = randomConfiguration(setup.Robot);
 
-result = J_inverse_kinematics(setup.Robot, config_a, config_b, setup.LastJointIndex, 0.01, 0.001, 50)
+result = J_inverse_kinematics(setup.Robot, config_a, test_config2, setup.LastJointIndex, 0.01, 0.001, 50)
 % result = J_transpose_kinematics(setup.Robot, config_a, config_b, setup.LastJointIndex, 0.01, 50, 0.5)
 
 animate(setup.Robot, setup.LastJointIndex, result.Configurations, "animations/test.mp4")
