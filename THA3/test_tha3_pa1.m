801aa8c5e79002412e51bed2aa632d12dcffed59
Elliott Turner <ertnxt@gmail.com>
2024-04-18 13:14:06 -0500

    also write output files for debug data for comparison

--- THA3/test_tha3_pa1.m
+++ THA3/test_tha3_pa1.m
@@ -7,16 +7,23 @@ unknown_letters = ['h','i','j','k'];
 
 % test again debug outputs
 for iDebug = 1:length(debug_letters)
-    output = tha3_pa1(dir,strcat('pa1-debug-',debug_letters(iDebug)));
-    [~,correct_output] = read_file(dir, strcat('pa1-debug-',debug_letters(iDebug),'-output1.txt'));
+    name = strcat('pa1-debug-',debug_letters(iDebug));
+    output = tha3_pa1(dir,name);
+    [~,correct_output] = read_file(dir, strcat(name,'-output1.txt'));
     max(output.data - correct_output)
+    write_file(outputDir,name,output);
 end
 
 % write unknown outputs
 for iUnknown = 1:length(unknown_letters)
-    output = tha3_pa1(dir,strcat('pa1-unknown-',unknown_letters(iUnknown)));
-    outputFilename = strcat('pa1-unknown-',unknown_letters(iUnknown),'-output1.txt');
-    fid = fopen(fullfile(outputDir, outputFilename),'w');
+    name = strcat('pa1-unknown-',unknown_letters(iUnknown));
+    output = tha3_pa1(dir,name);
+    write_file(outputDir,name,output);
+end
+
+function write_file(dir,name,output)
+    outputFilename = strcat(name,'-output1.txt');
+    fid = fopen(fullfile(dir, outputFilename),'w');
     fprintf(fid,'%d,%d,%s\n',[output.N_C,output.N_frames,outputFilename]);
     for i = 1:size(output.data)
         fprintf(fid,'%0.2f,%0.2f,%0.2f\n',output.data(i,:));
