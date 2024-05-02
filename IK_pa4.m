8ea11f01a4ddab0828632e9319f3c5e0d079e931
Elliott Turner <ertnxt@gmail.com>
2024-05-02 03:41:58 -0500

    add orientation control that seems to sorta work?

--- IK_pa4.m
+++ IK_pa4.m
@@ -1,6 +1,6 @@
 % keeps tool tip as close as possible to p_goal while never going
 % tol away from the goal and obeying joint limits
-function result = IK_pa4_a(robotRBT,lastJointIndex,config_a,p_goal,tol,maxIterations)
+function result = IK_pa4_a(robotRBT,lastJointIndex,config_a,p_goal,tol,maxIterations,zeta,eta)
     i = 1;
     config_b = config_a;
     configurations = {};
@@ -25,7 +25,11 @@ function result = IK_pa4_a(robotRBT,lastJointIndex,config_a,p_goal,tol,maxIterat
         J_e = J(4:6,:);
         A = -sk(T(1:3,4))*J_a + J_e;
         b = p_goal - T(1:3,4);
-        config_delta = A\b;
+        config_delta_position = A\b;
+        A = -sk(T(1:3,1:3)*[0;0;1])*J_a;
+        b = [0;0;0];
+        config_delta_orientation = A\b;
+        config_delta = zeta*config_delta_position + eta*config_delta_orientation;
 
         % update configuration and enforce joint limits
         for j = 1:lastJointIndex-1
